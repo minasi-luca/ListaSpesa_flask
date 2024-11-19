ccc62df27ca00b5ed8b52b2520fca8c53805e007
minasi-luca <minasi.luca@iisgalvanimi.edu.it>
2024-11-19 08:45:36 +0000

    terzo

--- ListaSpesa_flask/app.py
+++ ListaSpesa_flask/app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
-#inizializza l'app Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
-#rotta principale
+
+
+lista_spesa = []
+
 @app.route('/')
-def home():
-    return "Per ora funziona tutto"
-#avvio dell'app Flask
+def index():
+
+    return render_template('index.html', lista=lista_spesa)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+
+    elemento = request.form.get('elemento')
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('index'))
+
+@app.route('/rimuovi/<int:indice>')
+def rimuovi(indice):
+
+    if 0 <= indice < len(lista_spesa):
+        lista_spesa.pop(indice)
+    return redirect(url_for('index'))
+
+@app.route('/svuota')
+def svuota():
+
+    lista_spesa.clear()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     app.run(debug=True)
